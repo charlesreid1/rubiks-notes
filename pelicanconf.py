0927eaf50c5665c7f1f5824ceda360c034f6fcf3
Charles Reid <charlesreid1@gmail.com>
2018-01-04 03:19:24 -0800

    update site-url

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,11 +16,11 @@ import glob
 # -----------------------
 # SET THIS!!!!!1!11!!!!!!!!!!
 
-# Local viewing:
-SITEURL = ''
+### # Local viewing:
+### SITEURL = ''
 
-### # Github hosting
-### SITEURL = 'rubiks-notes'
+# Github hosting
+SITEURL = 'rubiks-notes'
 
 # OK TNX BYE
 # ----------------------
